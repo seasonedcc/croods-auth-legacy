4b8427000782f45e393c952b1f800c8cfdfa6370
vractal <v@fractopia.org>
2019-01-25 12:36:00 -0200

    Export credentials and headers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import ImportedAuth from './Auth'
 import importedCreateReducer from './createReducer'
+import importedAfterResponse from './credentials/afterResponse'
+import importedHeaders from './credentials/headers'
+import importedSignIn from './signIn/SignIn'
 
 export const Auth = ImportedAuth
 export const createReducer = importedCreateReducer
+export const headers = importedHeaders
+export const afterResponse = importedAfterResponse
+export const SignIn = importedSignIn
